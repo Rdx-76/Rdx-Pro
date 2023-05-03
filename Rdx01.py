f564f19e55046ee8347773a591ee59263cf3d015
Rdx-Vai <132105892+Rdx-vai@users.noreply.github.com>
2023-05-04 00:07:45 +0600

    Delete Rdx01.py

--- Rdx01.py
+++
@@ -1,9 +0,0 @@
-import os,platform
-os.system('git pull')
- 
-Rdx=platform.architecture()[0]
-if Rdx=="32bit":
-    print('Sorry 32 Bit Not Supported...')
-elif Rdx=="64bit":
-
-    __import__("Rdx")
