2fdfe9a4578607192d550207da4bc0035c9b7e64
Rdx-Vai <132105892+Rdx-vai@users.noreply.github.com>
2023-05-04 00:00:24 +0600

    Update Rdx01.py

--- Rdx01.py
+++ Rdx01.py
@@ -5,5 +5,5 @@ Rdx=platform.architecture()[0]
 if Rdx=="32bit":
     print('Sorry 32 Bit Not Supported...')
 elif Rdx=="64bit":
-    #print('Command is in update wait we will fix it soon !')
+
     __import__("Rdx")
